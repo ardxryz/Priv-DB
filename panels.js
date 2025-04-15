b4a667e3fe99727ac5be6b4826194836247a4684
ardxryz <ardhitahosting5@gmail.com>
2025-04-16 00:01:39 +0700

    Update panels.js

--- panels.js
+++ panels.js
@@ -4,6 +4,11 @@
       "username": "ArdBotzz",
       "password": "tybro",
       "ip_whitelist": ["167.71.71.176"]
+    },
+    {
+      "username": "Roxxie",
+      "password": "Crazybotzzz",
+      "ip_whitelist": ["102.56.82.108"]
     }
   ]
 }
