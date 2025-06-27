e22c1bffc851e9d524fc4f278af93f49d62f3d1a
ardxryz <ardhitahosting5@gmail.com>
2025-06-28 02:11:49 +0700

    Update panels.js

--- panels.js
+++ panels.js
@@ -6,8 +6,8 @@
       "ip_whitelist": ["167.71.71.176"]
     },
     {
-      "username": "Roxxie",
-      "password": "Crazybotzzz",
+      "username": "ROXXIE",
+      "password": "CRAZYDEV",
       "ip_whitelist": ["102.56.82.108"]
     }
   ]
